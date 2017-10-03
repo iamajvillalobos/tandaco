56b8926e30e10726d5037d6c826d9fc12f4c4c4d
Arman Jon Villalobos <me@armanjon.com>
2017-10-03 18:06:47 +0800

    Setup testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,15 @@
-require "bundler/setup"
-require "tandaco"
+require 'bundler/setup'
+require 'rspec'
+require 'tandaco'
+require 'pathname'
+
+SPEC_DIR = Pathname.new(File.dirname(__FILE__))
+Dir[SPEC_DIR.join('support', '*.rb')].each { |f| require f }
+FIXTURES_DIR = SPEC_DIR.join('fixtures')
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
--- tandaco.gemspec
+++ tandaco.gemspec
@@ -19,10 +19,13 @@ Gem::Specification.new do |spec|
   spec.require_paths = ['lib']
 
   spec.add_development_dependency 'bundler', '~> 1.15'
+  spec.add_development_dependency 'factory_girl', '~> 4.8'
+  spec.add_development_dependency 'pry', '~> 0.11'
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_development_dependency 'rspec', '~> 3.0'
+  spec.add_development_dependency 'vcr', '~> 3.0'
+  spec.add_development_dependency 'webmock', '~> 3.0'
 
   spec.add_dependency 'api_client_base', '~> 1.0'
   spec.add_dependency 'typhoeus', '~> 1.0'
-  spec.add_dependency 'pry'
 end
